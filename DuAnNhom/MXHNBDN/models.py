f37146d1fe7f45c4a6128963c78528306aae169e
kingofforeverr <nguyetha1234hn@gmail.com>
2025-04-05 16:07:36 +0700

    update models v2

--- DuAnNhom/MXHNBDN/models.py
+++ DuAnNhom/MXHNBDN/models.py
@@ -1,12 +1,27 @@
-from django.db import models
-from django.db import models
-from django.contrib.auth.models import AbstractUser
 
-# Người dùng
+from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.db import models
+
 class NguoiDung(AbstractUser):
     so_dien_thoai = models.CharField(max_length=15, unique=True)
     email = models.EmailField(unique=True)
     avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+
+    groups = models.ManyToManyField(
+        Group,
+        related_name='nguoidung_set',
+        blank=True,
+        help_text='The groups this user belongs to.',
+        verbose_name='groups',
+    )
+
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='nguoidung_permissions',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions',
+    )
 
 # Tài khoản (đã gộp vào AbstractUser)
 
