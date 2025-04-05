7ae431b7a33d766dcc1c1f407afa061468f5e065
nagashi456 <anhphucnguyen2004@gmail.com>
2025-04-05 20:56:25 +0700

    hello

--- DuAnNhom/MXHNBDN/views.py
+++ DuAnNhom/MXHNBDN/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
-def index(request):
+def Binhluan(request):
     return render(request,"TaoBinhLuan/Taobinhluan.html")
+def edit_profile(request):
+    return render(request,"Edit_profile/edit_profile.html")
 #1231234214124
