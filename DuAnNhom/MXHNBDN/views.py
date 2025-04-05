e5d3716c2a0b0df02fe5328edffe3324fc097b51
teame <nguyentramy24012004@gmail.com>
2025-04-05 21:46:56 +0700

    hello

--- DuAnNhom/MXHNBDN/views.py
+++ DuAnNhom/MXHNBDN/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 def index(request):
-    return render(request,"TaoBinhLuan/Taobinhluan.html")
+    return render(request,"TaoBinhChon/TaoBinhChon.html")
 #1231234214124
