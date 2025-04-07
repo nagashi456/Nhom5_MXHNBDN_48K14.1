fd1cc1632b332318b23090bcf501e7a406b9b80b
teame <nguyentramy24012004@gmail.com>
2025-04-07 15:54:24 +0700

    hello

--- DuAnNhom/MXHNBDN/urls.py
+++ DuAnNhom/MXHNBDN/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('binh_luan/', views.Binhluan, name='binhluan'),
+    path('binh_chon/', views.BinhChon, name='binhchon'),
     path('edit_profile/', views.edit_profile, name='edit_profile'),
 ]
--- DuAnNhom/MXHNBDN/views.py
+++ DuAnNhom/MXHNBDN/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
-def index(request):
+def Binhluan(request):
     return render(request,"TaoBinhLuan/Taobinhluan.html")
+def BinhChon(request):
+    return render(request,"TaoBinhChon/TaoBinhChon.html")
+def edit_profile(request):
+    return render(request,"Edit_profile/edit_profile.html")
 #1231234214124
