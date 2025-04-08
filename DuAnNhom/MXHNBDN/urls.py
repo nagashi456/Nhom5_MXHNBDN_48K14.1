dbe1c2ab7ad3ef59f3ddf8a2f43e2c4475c8dd30
kingofforeverr <nguyetha1234hn@gmail.com>
2025-04-08 12:49:36 +0700

    update profile detail

--- DuAnNhom/MXHNBDN/urls.py
+++ DuAnNhom/MXHNBDN/urls.py
@@ -3,4 +3,7 @@ from . import views
 urlpatterns = [
     path('binh_luan/', views.Binhluan, name='binhluan'),
     path('edit_profile/', views.edit_profile, name='edit_profile'),
+    path('',views.index,name='index'),
+    path('profile_details/',views.detail_profile, name = 'profile_details'),
+
 ]
--- DuAnNhom/MXHNBDN/views.py
+++ DuAnNhom/MXHNBDN/views.py
@@ -3,4 +3,8 @@ def Binhluan(request):
     return render(request,"TaoBinhLuan/Taobinhluan.html")
 def edit_profile(request):
     return render(request,"Edit_profile/edit_profile.html")
-#1231234214124
+
+def index(request):
+    return render(request,"base.html")
+def detail_profile(request):
+    return render(request,"Edit_profile/profile_details.html")
