aec4ab507b8cdf3689112122bd109bae5bebf085
anhdth <Ntramanh2004@gmail.com>
2025-04-08 14:17:57 +0700

    update

--- DuAnNhom/MXHNBDN/views.py
+++ DuAnNhom/MXHNBDN/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 def Binhluan(request):
     return render(request,"TaoBinhLuan/Taobinhluan.html")
+def BinhChon(request):
+    return render(request,"TaoBinhChon/TaoBinhChon.html")
 def edit_profile(request):
     return render(request,"Edit_profile/edit_profile.html")
-#1231234214124
+def Nhantin(request):
+    return render(request,"NhanTin/NhanTin.html")
