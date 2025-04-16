e2c5eb2121e38b97e0f753e7488be59a736e3e21
nagashi456 <anhphucnguyen2004@gmail.com>
2025-04-16 10:31:32 +0700

    hello

--- DuAnNhom/MXHNBDN/urls.py
+++ DuAnNhom/MXHNBDN/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     path('nhan_tin/', views.Nhantin, name='nhantin'),
     path('tao_tai_khoan/',views.TaoTaiKhoan,name='tao_tai_khoan'),
     path('profile_detail/',views.ProfileDetail,name='profile_detail'),
-    path('login/',views.DangNhap,name='dang_nhap')
+    path('login/',views.DangNhap,name='dang_nhap'),
+    path('trang_chu/',views.Trangchu,name='trang_chu'),
+    path('tao_bai_viet/',views.TaoBaiViet,name='tao_bv'),
+    path('sua_bai_viet/',views.SuaBaiViet,name='sua_bv'),
+
 ]
