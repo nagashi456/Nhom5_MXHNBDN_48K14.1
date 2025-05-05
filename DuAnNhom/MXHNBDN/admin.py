b3be962a60da6d834bdddbbe24bb41de79b0bed1
nagashi456 <anhphucnguyen2004@gmail.com>
2025-05-06 00:32:57 +0700

    up admin

--- DuAnNhom/MXHNBDN/admin.py
+++ DuAnNhom/MXHNBDN/admin.py
@@ -1,72 +1,90 @@
 from django.contrib import admin
-from .models import (
-    NguoiDung,
-    BaiViet, BinhLuan,
-    CuocTroChuyen, TinNhan,
-    BinhChon, LuaChonBinhChon, BinhChonNguoiDung,
-    Nhom, ThanhVienNhom, CauHoi,
+from . import models
 
-)
-from django.contrib.auth.admin import UserAdmin
+@admin.register(models.PhongBan)
+class PhongBanAdmin(admin.ModelAdmin):
+    list_display = ('id', 'TenPhong', 'HinhAnhPhong')
+    search_fields = ('TenPhong',)
 
-@admin.register(NguoiDung)
-class NguoiDungAdmin(UserAdmin):
-    model = NguoiDung
-    list_display = ('username', 'email', 'so_dien_thoai', 'vai_tro', 'is_staff', 'is_active')
-    list_filter = ('vai_tro', 'is_staff', 'is_superuser', 'is_active', 'groups')
-    search_fields = ('username', 'email', 'so_dien_thoai')
-    ordering = ('username',)
+@admin.register(models.NguoiDung)
+class NguoiDungAdmin(admin.ModelAdmin):
+    list_display = ('id', 'HoTen', 'SoDienThoai', 'Email', 'MaPhong', 'user')
+    list_filter = ('MaPhong',)
+    search_fields = ('HoTen', 'Email', 'SoDienThoai')
 
-    fieldsets = UserAdmin.fieldsets + (
-        ("Thông tin bổ sung", {
-            "fields": ("so_dien_thoai", "avatar", "vai_tro"),
-        }),
-    )
-
-
-@admin.register(BaiViet)
+@admin.register(models.BaiViet)
 class BaiVietAdmin(admin.ModelAdmin):
-    list_display = ('nguoi_dung', 'ngay_tao')
-    search_fields = ('noi_dung',)
+    list_display = ('id', 'MaNguoiDung', 'ThoiGianTao')
+    list_filter = ('MaNguoiDung',)
+    search_fields = ('NoiDung',)
 
-@admin.register(BinhLuan)
+@admin.register(models.TepDinhKem)
+class TepDinhKemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'Tep', 'FileSize', 'MaBaiViet')
+    list_filter = ('MaBaiViet',)
+
+@admin.register(models.HinhAnh)
+class HinhAnhAdmin(admin.ModelAdmin):
+    list_display = ('id', 'Anh', 'ImgSize', 'MaBaiViet')
+    list_filter = ('MaBaiViet',)
+
+@admin.register(models.BinhLuan)
 class BinhLuanAdmin(admin.ModelAdmin):
-    list_display = ('nguoi_dung', 'bai_viet', 'ngay_tao')
-    search_fields = ('noi_dung',)
+    list_display = ('id', 'MaNguoiDung', 'MaBaiViet', 'NgayTao')
+    list_filter = ('MaNguoiDung',)
+    search_fields = ('NoiDung',)
 
-@admin.register(CuocTroChuyen)
+@admin.register(models.LuotCamXuc)
+class LuotCamXucAdmin(admin.ModelAdmin):
+    list_display = ('id', 'MaNguoiDung', 'MaBaiViet', 'is_like', 'ThoiGian')
+    list_filter = ('is_like',)
+
+@admin.register(models.CuocTroChuyen)
 class CuocTroChuyenAdmin(admin.ModelAdmin):
-    list_display = ('id', 'ten_nhom')
-    filter_horizontal = ('thanh_vien',)
+    list_display = ('id', 'Loai', 'TenNhom')
+    search_fields = ('Loai', 'TenNhom')
 
-@admin.register(TinNhan)
-class TinNhanAdmin(admin.ModelAdmin):
-    list_display = ('nguoi_gui', 'cuoc_tro_chuyen', 'ngay_tao')
+@admin.register(models.ThanhVienCuocTroChuyen)
+class ThanhVienCuocTroChuyenAdmin(admin.ModelAdmin):
+    list_display = ('id', 'MaCuocTroChuyen', 'MaNguoiDung', 'NgayGiaNhap')
+    list_filter = ('MaCuocTroChuyen',)
+
+@admin.register(models.TinNhanChiTiet)
+class TinNhanChiTietAdmin(admin.ModelAdmin):
+    list_display = ('id', 'NguoiDung', 'MaCuocTroChuyen', 'NgayTao')
+    list_filter = ('NguoiDung',)
+    search_fields = ('NoiDung',)
+
+@admin.register(models.BangHoi)
+class BangHoiAdmin(admin.ModelAdmin):
+    list_display = ('id', 'MaNguoiDung', 'NgayTao')
+    list_filter = ('MaNguoiDung',)
+    search_fields = ('NoiDung',)
+
+@admin.register(models.BangTL)
+class BangTLAdmin(admin.ModelAdmin):
+    list_display = ('id', 'MaHoi', 'MaNguoiDung', 'NgayTao')
+    list_filter = ('MaHoi',)
+    search_fields = ('NoiDung',)
+
+@admin.register(models.BinhChon)
+class BinhChonAdmin(admin.ModelAdmin):
+    list_display = ('id', 'TenTieuDe', 'MaNguoiDung', 'ThoiGianKetThucBC')
+    list_filter = ('MaNguoiDung',)
+    search_fields = ('TenTieuDe', 'MoTa')
+
+@admin.register(models.LuaChonBinhChon)
+class LuaChonBinhChonAdmin(admin.ModelAdmin):
+    list_display = ('id', 'binh_chon', 'noi_dung')
+    list_filter = ('binh_chon',)
     search_fields = ('noi_dung',)
 
-@admin.register(BinhChon)
-class BinhChonAdmin(admin.ModelAdmin):
-    list_display = ('ten_tieu_de', 'nguoi_tao', 'thoi_gian_ket_thuc')
-    search_fields = ('ten_tieu_de',)
-
-@admin.register(LuaChonBinhChon)
-class LuaChonBinhChonAdmin(admin.ModelAdmin):
-    list_display = ('binh_chon', 'noi_dung')
-
-@admin.register(BinhChonNguoiDung)
+@admin.register(models.BinhChonNguoiDung)
 class BinhChonNguoiDungAdmin(admin.ModelAdmin):
-    list_display = ('nguoi_dung', 'lua_chon')
+    list_display = ('id', 'nguoi_dung', 'lua_chon')
+    list_filter = ('nguoi_dung',)
 
-@admin.register(Nhom)
-class NhomAdmin(admin.ModelAdmin):
-    list_display = ('ten_nhom', 'ngay_tao')
-
-@admin.register(ThanhVienNhom)
-class ThanhVienNhomAdmin(admin.ModelAdmin):
-    list_display = ('nhom', 'nguoi_dung', 'ngay_gia_nhap')
-
-
-
-# Register your models here.
-
-# Register your models here.
+@admin.register(models.BinhChonNhom)
+class BinhChonNhomAdmin(admin.ModelAdmin):
+    list_display = ('id', 'MaPhong', 'MaBinhChon')
+    list_filter = ('MaPhong',)
